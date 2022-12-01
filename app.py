4cc1cb0394c9881e3ff932269443c257382dfc6d
sierra25 <lovejoy.sierra2@gmail.com>
2022-12-01 07:32:13 -0600

    moved break timer

--- app.py
+++ app.py
@@ -3,10 +3,43 @@ from random import randrange
 import json
 from dotenv import load_dotenv, find_dotenv
 import requests
-import flask
+from flask import Flask, render_template, request
 import random
+from flask_sqlalchemy import SQLAlchemy
 
-app = flask.Flask(__name__)  # name is the module name
+
+app=Flask(__name__)  # name is the module name, it is where the app should start running
+
+
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:sL74217513@localhost/tim'
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.secret_key = 'secret string'
+
+db = SQLAlchemy(app)
+
+
+
+
+
+class Event(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    activity = db.Column(db.String(80), unique=True, nullable=False)
+
+    #def __repr__(self): # this will give it a string representation of itself
+     #   return f'Event: {self.activity}' # this f string allows you to inject python variables
+
+    def __init__(self, activity):
+        self.activity= activity
+        
+
+
+
+    
+
+
+
+
 
 load_dotenv(find_dotenv())  # This is to load your API keys from .env
 
@@ -33,7 +66,7 @@ def break_screen():
     char_data = json_data
     image_url = char_data["image"]
 
-    return flask.render_template(
+    return render_template(
         "break.html",
         showurl=tv_url,
         title=tv_title,
@@ -44,14 +77,38 @@ def break_screen():
 
 @app.route("/work")
 def work_screen():
-    return flask.render_template("work.html")
+    return render_template("work.html")
 
 @app.route("/")
 def index():
-    return flask.render_template("index.html")  #main page
+    return render_template("index.html")
+#def home():
+#    return '<a href="/addperson"><button> Click here </button></a>'
+#  #main page
     #return flask.redirect(flask.url_for("break_screen")) #break page
 
 
-app.run(
-    debug=True
-)  # I will not need to reload the page each time I make changes with the debug=True
+@app.route("/addperson")
+def addperson():
+    return render_template("index.html")
+
+@app.route("/personadd", methods=['POST'])
+def personadd():
+    activity = request.form["activity"]
+    entry = Event(activity)
+#   db.session.add(entry)
+#    db.session.commit()
+
+    return render_template("index.html")
+
+
+
+
+
+
+if __name__ == '__main__':
+#    with app.app_context():
+#        db.create_all()
+        app.run(
+            debug=True
+        )  # I will not need to reload the page each time I make changes with the debug=True
